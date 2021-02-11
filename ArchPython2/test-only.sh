dcb31d2b47fa3eab3d22b414c27093fa08e2b5a9
than97 <thomas.h@pervices.com>
2021-02-11 09:10:03 -0500

    Edied test-only.sh for Python2 to remove initial commands that were causing issues

--- ArchPython2/test-only.sh
+++ ArchPython2/test-only.sh
@@ -14,7 +14,7 @@ TEST_RESULTS=()
 RETURN=0
 HOST=$(hostname)
 USER=$(whoami)
-NUMER_OF_EN_DEVICES=$(ifconfig -a | grep enp | wc -l)
+#NUMER_OF_EN_DEVICES=$(ifconfig -a | grep enp | wc -l)
 PV_HOST=192.168.10.
 PV_INT=192.168.128.
 SFP0=10.10.11.10
@@ -28,72 +28,13 @@ DUT_DATA_IP_ADDR2=10.10.11.2
 echo ":: Starting preparation work"
 
 cd .. &&
-mkdir -p pkg-archive && touch 1.xz && mv *.xz pkg-archive/ && sync &&
-echo $USER"@"$HOST | sudo -S rm -fv /usr/bin/usrp2_card_burner /usr/lib/uhd/utils/usrp2_card_burner.py /usr/lib/uhd/utils/usrp2_recovery.py &&
-makepkg &&
-echo $USER"@"$HOST | sudo -S pacman -U --noconfirm *.pkg.tar.xz &&
-cp src/uhd/host/build/examples/test_tx_trigger tests/ && 
-mv *.pkg.tar.xz pkg-archive/ && sync && cd scripts &&
+#mkdir -p pkg-archive && touch 1.xz && mv *.xz pkg-archive/ && sync &&
+#echo $USER"@"$HOST | sudo -S rm -fv /usr/bin/usrp2_card_burner /usr/lib/uhd/utils/usrp2_card_burner.py /usr/lib/uhd/utils/usrp2_recovery.py &&
+#makepkg &&
+#echo $USER"@"$HOST | sudo -S pacman -U --noconfirm *.pkg.tar.xz &&
+#cp src/uhd/host/build/examples/test_tx_trigger tests/ && 
+#mv *.pkg.tar.xz pkg-archive/ && sync && cd scripts &&
 
-echo ":: Configuring environment - UHD should eventually do this automatically."
-
-echo $USER"@"$HOST | sudo -S sysctl -w net.core.rmem_max=50000000 &&
-echo $USER"@"$HOST | sudo -S sysctl -w net.core.wmem_max=2500000 &&
-
-for i in $(seq 1 $NUMER_OF_EN_DEVICES)
-do
-	NAME=$(ifconfig -a | grep enp | awk '{print $1}' | cut -d ':' -f1 | sed -n ${i}p)
-	IP_INTERNAL=$(ifconfig $NAME | grep $PV_INT | awk '{print $2}')
-	IP_HOST=$(ifconfig $NAME | grep $PV_HOST | awk '{print $2}')
-	IP_SFP0=$(ifconfig $NAME | grep $SFP0 | awk '{print $2}')
-	IP_SFP1=$(ifconfig $NAME | grep $SFP1 | awk '{print $2}')
-
-	if [ -n "$IP_INTERNAL" ]
-	then
-		COUNT=$(($COUNT+1))
-		echo "Interface $NAME has PV internal ip address $IP_INTERNAL" &&
-		echo "Setting Interface $NAME MTU size to 9000..." &&
-		echo $USER"@"$HOST | sudo -S ip link set $NAME up mtu 9000 &&
-		echo ""
-	fi
-
-	if [ -n "$IP_HOST" ]
-	then
-		COUNT=$(($COUNT+1))
-		echo "Interface $NAME has target device host ip address $IP_HOST" &&
-		echo "Setting Interface $NAME MTU size to 9000..." &&
-		echo $USER"@"$HOST | sudo -S ip link set $NAME up mtu 9000 &&
-		echo ""
-	fi
-
-	if [ -n "$IP_SFP0" ]
-	then
-		COUNT=$(($COUNT+1))
-		echo "Interface $NAME has SFP0 ip address $IP_SFP0" &&
-		echo "Setting Interface $NAME MTU size to 9000..." && 
-		echo $USER"@"$HOST | sudo -S ip link set $NAME up mtu 9000 &&
-		echo ""
-	fi
-
-	if [ -n "$IP_SFP1" ]
-	then
-		COUNT=$(($COUNT+1))
-		echo "Interface $NAME has SFP1 ip address $IP_SFP1" &&
-		echo "Setting Interface $NAME MTU size to 9000..." &&
-		echo $USER"@"$HOST | sudo -S ip link set $NAME up mtu 9000 &&
-		echo ""
-	fi
-done
-
-if [ "$COUNT" != "4" ]
-then
-		echo "Failed to set network properties. None of the following addresses should be empty:"
-		echo "PV Internal IP address is $IP_INTERNAL"
-		echo "Host IP address is $IP_HOST"
-		echo "SFP0 IP address is $IP_SFP0"
-		echo "SFP1 IP address is $IP_SFP1"
-		exit 1
-fi
 
 echo ":: Testing reachablity to DUT ::"
 
