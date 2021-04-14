e38f35d206e8f433ae34a96fccd628e3b5d737e8
than97 <thomas.h@pervices.com>
2021-04-14 12:15:10 -0400

    Editing gnuradio and volk build directories for Ubuntu, adding directory specifically for tests

--- ubuntu/test-only.sh
+++ ubuntu/test-only.sh
@@ -54,7 +54,7 @@ do
 	echo ":: Executing ${TEST_NAMES[$i]} test" >> log.txt
 	#echo "$i"
 	pwd
-	python3 -u /home/notroot/libuhd/tests/${TEST_FILES[$i]}.py
+	python3 -u /home/notroot/testing/${TEST_FILES[$i]}.py
 	rv=$?
 	if [ $rv -eq 0 ]; then
 		PASSED_TESTS=$((PASSED_TESTS+1))
