f5f0fdb66f2b91779ebeb7442fe10259821ca55b
DougCodeProjects <douglas.wilson12@ontariotechu.net>
2021-06-10 19:50:01 +0000

    Created a workaround for the GNU radio bug that causes start delays to have no effect. The workaround consists of taking samples equal to 1.5 times the sample rate and only using the last of the samples

--- Testing/tests/test_tx_rx_gain.py
+++ Testing/tests/test_tx_rx_gain.py
@@ -8,17 +8,24 @@ def main(iterations):
 
     # Collect.
     vsnks = []
+
+    sample_count = 0
+
     for it in iterations:
         gen.dump(it)
-        tx_stack = [ (5, it["sample_rate" ]) ] # One seconds worth.
-        rx_stack = [ (5, it["sample_count"]) ]
+        #Due to a bug that makes start times have no effect, tx will run for 3 seconds, rx will run for 1.5 seconds but only use the last sample_count samples
+        sample_count = it["sample_count"]
+        tx_stack = [ (10, int(it["sample_rate" ])*3) ]
+        rx_stack = [ (10.5, int(it["sample_count"] + (it["sample_rate" ])*1.5)) ]
         vsnk = engine.run(it["channels"], it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
         vsnks.append(vsnk)
 
     # Calculate absolute area.
     areas = []
     for vsnk in vsnks:
-        area = [sigproc.absolute_area(channel.data()) for channel in vsnk]
+        for channel in vsnk:
+            #Uses only the list sample_count sample, this should be changed to use all samples (which should be equal to sample_count) once theissues with tx and rx start delays are fixed
+            area = [sigproc.absolute_area(channel.data()[-sample_count:-1])]
         areas.append(area)
     areas = np.array(areas).T.tolist() # Transpose.
 
@@ -28,8 +35,8 @@ def main(iterations):
         assert area == sorted(area)
 
 
-main(gen.lo_band_gain_tx(1))
-main(gen.lo_band_gain_rx(1))
-main(gen.hi_band_gain_tx(1))
-main(gen.hi_band_gain_rx(1))
+main(gen.lo_band_gain_tx(4))
+main(gen.lo_band_gain_rx(4))
+main(gen.hi_band_gain_tx(4))
+main(gen.hi_band_gain_rx(4))
 
