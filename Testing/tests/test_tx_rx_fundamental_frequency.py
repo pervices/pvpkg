e5123d8f70f575e1d03b12182478b5ee7e128f98
Brendon McHugh <brendon.mch@pervices.com>
2021-12-09 21:04:01 +0000

    fundamental frequency test works

--- Testing/tests/test_tx_rx_fundamental_frequency.py
+++ Testing/tests/test_tx_rx_fundamental_frequency.py
@@ -4,19 +4,22 @@ from common import engine
 from common import generator as gen
 from retrying import retry
 import numpy as np
+import matplotlib.pyplot as plt
 
-@retry(stop_max_attempt_number = 3)
+@retry(stop_max_attempt_number = 1)
 def test(it):
     gen.dump(it)
 
 
-    tx_stack = [ (10.0, it["sample_rate" ]) ] # One seconds worth.
-    rx_stack = [ (10.5, it["sample_count"]) ]
+    tx_stack = [ (10.0, it["sample_count"]) ] # One seconds worth.
+    rx_stack = [ (10.0, int(it["sample_count"]) ) ]
     vsnk = engine.run(it["channels"], it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
 
     for ch, channel in enumerate(vsnk):
         real = [datum.real for datum in channel.data()]
         imag = [datum.imag for datum in channel.data()]
+        print(len(real))
+        print(len(imag))
 
         fund_real = sigproc.fundamental(real, it["sample_rate"])
         fund_imag = sigproc.fundamental(imag, it["sample_rate"])
@@ -28,12 +31,32 @@ def test(it):
 
         sigproc.dump_file(vsnk, it["wave_freq"])
 
+
         try:
             assert (like_real > 0.95 and like_real < 1.05 and like_imag > 0.95 and like_imag < 1.05), "like_real and like_imag do not meet requirements, fail"
         except:
             sigproc.dump(vsnk)
             raise
+            #plot and save real/imag component vs. sample
+            plt.figure()
+            plt.title("Fundamental frequency sample plot of {} for wave_freq = {} Hz".format(ch,it["wave_freq"]))
+            plt.xlabel("Sample")
+            plt.ylabel("Amplitude")
+            plt.plot(real[0:300], label='real')
+            plt.plot(imag[0:300],label='imag')
+            plt.legend()
+            plt.savefig(fname='Fundamental frequency sample plot for channel {} at wave_freq {}'.format(ch, it["wave_freq"],format='png'))
 
+            time=np.arange(0,len(real)/it["sample_rate"], 1/it["sample_rate"])
+            #plot and save real/imag component vs. time
+            plt.figure()
+            plt.title("Fundamental frequency time plot of {} for wave_freq = {} Hz".format(ch,it["wave_freq"]))
+            plt.xlabel("time")
+            plt.ylabel("Amplitude")
+            plt.plot(time, real, label='real')
+            plt.plot(time, imag, label='imag')
+            plt.legend()
+            plt.savefig(fname='Fundamental frequency time plot for channel {} at wave_freq {}'.format(ch, it["wave_freq"],format='png'))
 
 def main(iterations):
 
@@ -42,4 +65,3 @@ def main(iterations):
 
 
 main(gen.hi_band_wave_sweep())
-
