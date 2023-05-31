7216f368dee707df37120562f9e166527b048e22
Aleena Paul <aleena.p@pervices.com>
2023-05-31 13:45:58 +0000

    Commit for correcting the iterations

--- Testing/tests/test_phase_coherency.py
+++ Testing/tests/test_phase_coherency.py
@@ -6,24 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-def test(it):
+def main(iterations):
+    for it in iterations:
+        gen.dump(it)
 
-    gen.dump(it)
+        # Collect.
+        tx_stack = [ (10.0, it["sample_count" ]) ] # One seconds worth.
+        rx_stack = [ (10.0, it["sample_count"]) ]
+        vsnk = engine.run(it["channels"], it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
+        error_detected = 0
+        # Process.
 
-    # Collect.
-    tx_stack = [ (10.0, it["sample_count" ]) ] # One seconds worth.
-    rx_stack = [ (10.0, it["sample_count"]) ]
-    vsnk = engine.run(it["channels"], it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
-    error_detected = 0
-    # Process.
+        for ch, channel in enumerate(vsnk):
 
-    for ch, channel in enumerate(vsnk):
-
-        real = [datum.real for datum in channel.data()]
-        imag = [datum.imag for datum in channel.data()]
-        #print('the value of the real array is', real)
-        #print('the value of the imag array is', imag)
-        for b in range(len(it["channels"])):
+            real = [datum.real for datum in channel.data()]
+            imag = [datum.imag for datum in channel.data()]
+            #print('the value of the real array is', real)
+            #print('the value of the imag array is', imag)
+            for b in range(len(it["channels"])):
                 #print('range is',b)
                 time=np.arange(0,len(real)/it["sample_rate"], 1/it["sample_rate"])
                 plt.figure()
@@ -34,12 +34,9 @@ def test(it):
                 plt.plot(time[0:500], imag[0:500], color='green', label='imag')
                 plt.legend()
                 plt.savefig(fname='Time plot for channel {} at wave_freq {}'.format(b,it["wave_freq"],format='png'))
-                sys.exit(1)
+                #sys.exit(1)
 
-def main(iterations):
 
-    for it in iterations:
-        test(it)
 
 main(gen.lo_band_tx_ph_coherency(4))
 main(gen.lo_band_rx_ph_coherency(4))
