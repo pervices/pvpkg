6eec74b0d2261166706dcf0e0d3c7578584af450
Aleena Paul <aleena.p@pervices.com>
2023-06-02 15:14:46 +0000

    Commit for resolving errors in the fit function

--- Testing/tests/test_phase_coherency.py
+++ Testing/tests/test_phase_coherency.py
@@ -6,46 +6,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import sys
-def fit_func(xdata, A, lag_abs):
-    fit_y = A*np.exp((2*np.pi)+lag_abs)
+
+def fit_func(xdata, A, f_wave, abs_phase):
+    fit_y = A*np.sin(2*np.pi*f_wave*xdata+abs_phase)
     return fit_y
+
 def main(iterations):
     for it in iterations:
         gen.dump(it)
         # Collect.
-        tx_stack = [ (10, it["sample_count" ]) ] # One seconds worth.
+        tx_stack = [ (10, it["sample_rate" ]) ] # One seconds worth.
         rx_stack = [ (10.5, it["sample_count"]) ]
         vsnk = engine.run(it["channels"], it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
         error_detected = 0
-
         for ch, channel in enumerate(vsnk):
 
             real = [datum.real for datum in channel.data()]
             imag = [datum.imag for datum in channel.data()]
 
-
-            lag = sigproc.lag(real, imag, it["sample_rate"], it["wave_freq"])
-            lag_abs = abs(lag)
-
+            #w_freq = abs(it["wave_freq"])
             print("channel %2d: lag %f" % (ch, lag))
-            #print('the value of the real array is', real)
-            #print('the value of the imag array is', imag)
-            #for b in it["channels"]:fig, axs = plt.subplots(2, 2)
-            #print('it is',it["name"])
             time=np.arange(0,len(real)/it["sample_rate"], 1/it["sample_rate"])
             xdata = np.asarray(time)
-            rdata = np.asarray(real)
-            idata = np.asarray(imag)
-            f_data = idata*1j + real
-            #size = len(f_data)
-            #size_t = len(xdata)
-            #print("fdata xdata",size,size_t)
-            fit_data = fit_func(xdata, f_data, lag_abs)
-            p_opt,p_cov = curve_fit(fit_func, xdata, fit_data)
-            y_data = fit_func(xdata, *p_opt)
-            plt.plot(xdata, f_data, 'o')
-            plt.plot(xdata, y_data, '-', label='fit',color='red')
-            plt.savefig(fname='fitplot for time amp')
+            rdata = np.asarray(real)*np.sin(2*np.pi*wave_freq*xdata)
+            idata = rdata * 0
+
+            rdata_max = max(rdata) #Provides us with the max value.
+            rdata_norm = rdata * (1/rdata_max) #Normalizes nominal amplitude to 1
+            plt.plot(xdata, rdata_norm, 'o')
+            plt.savefig(fname='dataplot-for-time-amp')
+
+            guess_opt=[1, sample_rate, 1]
+            p_opt,p_cov = curve_fit(fit_func, xdata, rdata_norm, p0=guess_opt)
+            A=p_opt[0]
+            f_wave=p_opt[1]
+            abs_phase=p_opt[2]
+            fit_data = fit_func(xdata, A, f_wave, abs_phase)
+
+            plt.plot(xdata, fit_data, '-')
+            plt.savefig(fname='fitplot-for-time-amp')
 
             plt.figure()
             plt.title("Time plot of {} for wave_freq = {} Hz".format(ch,it["wave_freq"]))
