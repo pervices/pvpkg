1273456bd938aafb9387e720783f7b9bf0f61482
Shiqi Feng <shiqi.f@pervices.com>
2023-06-13 15:38:04 -0400

    Docker: Adding additional python modules for test report

--- Arch/GnuRadio/dkr.sh
+++ Arch/GnuRadio/dkr.sh
@@ -2,11 +2,8 @@
 
 PV_DOCKER=pvtesting/archlinux:20230322.0.136594-a72fd3ef-v3.10.6.0-rc1
 DISPLAY_NUMBER=$(echo $DISPLAY | cut -d. -f1 | cut -d: -f2)
-#echo $DISPLAY_NUMBER
 AUTH_COOKIE=$(xauth list | grep "^$(hostname)/unix:${DISPLAY_NUMBER} " | awk '{print $3}')
-#echo $AUTH_COOKIE
 DISPLAY_HOST=$DISPLAY
-#echo $DISPLAY_HOST
 dkr() {
     docker run --rm \
                --interactive \
@@ -14,10 +11,10 @@ dkr() {
                --tty \
                --env DISPLAY \
                --network host \
-	       --volume $(pwd):$(pwd) \
-	       --workdir $(pwd) \
-	       --env GGID=$(id -g) \
-	       --env UUID=$(id -u) \
+               --volume $(pwd):$(pwd) \
+               --workdir $(pwd) \
+               --env GGID=$(id -g) \
+               --env UUID=$(id -u) \
                $PV_DOCKER \
                /bin/bash -c \
                "xauth add $DISPLAY_HOST . $AUTH_COOKIE && $1 $2"
