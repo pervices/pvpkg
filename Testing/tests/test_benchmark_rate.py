de09b373ea47c93949fe1cec56130c89b8769953
Chen Jin <chen.j@pervices.com>
2023-09-27 22:21:38 +0000

    BENCHMARK, CI: tidied up code

--- Testing/tests/test_benchmark_rate.py
+++ Testing/tests/test_benchmark_rate.py
@@ -11,14 +11,15 @@ PATH = '/usr/lib/uhd/examples/benchmark_rate'
 def rx_benchmark(header='', duration=10, rx_rate=65e6, rx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, rx_otw=True, sc='sc16', rx_cpu="sc16", rx_delay=0):
     print('\n\n\n')
     print("*****************************\n")
-    print(header,'\n')
+    print('RX/TX', rx_cpu, 'multi-streamer' if multi_streamer else 'single','rx_delay='+str(rx_delay),'\n')
     print("*****************************\n\n")
+
     sample = subprocess.check_output([PATH, '--duration', str(duration), '--rx_rate', str(rx_rate), '--rx_channels', rx_channels, '--drop-threshold', str(drop_threshold), '--seq-threshold', str(seq_threshold),'--multi_streamer' if multi_streamer else '', '--rx_otw', sc, '--rx_cpu', rx_cpu, '--rx_delay', str(rx_delay)])
     sample = sample.decode()
+
     benchmark_summary = sample.split('Benchmark rate summary:\n')[1]
     results = benchmark_summary.split('\n')[0:10]
 
-    data_entry = {}
     for result in results:
         result = result.split(':')
         result_entry = result[0].lstrip()
@@ -41,14 +42,15 @@ def rx_benchmark(header='', duration=10, rx_rate=65e6, rx_channels="0,1,2,3", dr
 def tx_benchmark(header='', duration=10, tx_rate=65e6, tx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, tx_otw=True, sc='sc16', tx_cpu="sc16", tx_delay=5):
     print('\n\n\n')
     print("*****************************\n")
-    print(header,'\n')
+    print('TX', tx_cpu, 'multi-streamer' if multi_streamer else 'single', 'tx_delay='+str(tx_delay),'\n')
     print("*****************************\n\n")
+
     sample = subprocess.check_output([PATH, '--duration', str(duration), '--tx_rate', str(tx_rate), '--tx_channels', tx_channels, '--drop-threshold', str(drop_threshold), '--seq-threshold', str(seq_threshold),'--multi_streamer' if multi_streamer else '', '--tx_otw', sc, '--tx_cpu', tx_cpu, '--tx_delay', str(tx_delay)])
     sample = sample.decode()
+
     benchmark_summary = sample.split('Benchmark rate summary:\n')[1]
     results = benchmark_summary.split('\n')[0:10]
 
-    data_entry = {}
     for result in results:
         result = result.split(':')
         result_entry = result[0].lstrip()
@@ -64,17 +66,18 @@ def tx_benchmark(header='', duration=10, tx_rate=65e6, tx_channels="0,1,2,3", dr
             print('ERROR with rx_benchmark', result_entry, result_data);
             sys.exit(1)
 
-def rx_tx_benchmark(header='', duration=10, tx_rate=21666667, rx_rate=21666667, channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, tx_otw=True, sc='sc16', tx_cpu="sc16", rx_cpu='sc16', tx_delay=5, rx_delay=5):
+def rx_tx_benchmark(duration=10, tx_rate=21666667, rx_rate=21666667, channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, tx_otw=True, sc='sc16', tx_cpu="sc16", rx_cpu='sc16', tx_delay=5, rx_delay=5):
     print('\n\n\n')
     print("*****************************\n")
-    print(header,'\n')
+    print('RX/TX', tx_cpu, 'multi-streamer' if multi_streamer else 'single', 'tx_delay='+str(tx_delay),'rx_delay='+str(rx_delay),'\n')
     print("*****************************\n\n")
+
     sample = subprocess.check_output([PATH, '--duration', str(duration), '--tx_rate', str(tx_rate), '--rx_rate', str(rx_rate), '--channels', channels, '--drop-threshold', str(drop_threshold), '--seq-threshold', str(seq_threshold),'--multi_streamer' if multi_streamer else '', '--tx_otw', sc, '--tx_cpu', tx_cpu, 'rx-cpu', rx_cpu, '--tx_delay', str(tx_delay), '--rx_delay', str(rx_delay)])
     sample = sample.decode()
+
     benchmark_summary = sample.split('Benchmark rate summary:\n')[1]
     results = benchmark_summary.split('\n')[0:10]
 
-    data_entry = {}
     for result in results:
         result = result.split(':')
         result_entry = result[0].lstrip()
