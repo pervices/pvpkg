28ada6bc3a68813176c5d7ef5c03cf1aa7b81f2d
Chen Jin <chen.j@pervices.com>
2023-09-27 22:28:35 +0000

    BENCHMARK, CI: removed all header related codes

--- Testing/tests/test_benchmark_rate.py
+++ Testing/tests/test_benchmark_rate.py
@@ -8,7 +8,7 @@ PATH = '/usr/lib/uhd/examples/benchmark_rate'
 
 
 
-def rx_benchmark(header='', duration=10, rx_rate=65e6, rx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, rx_otw=True, sc='sc16', rx_cpu="sc16", rx_delay=0):
+def rx_benchmark(duration=10, rx_rate=65e6, rx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, rx_otw=True, sc='sc16', rx_cpu="sc16", rx_delay=0):
     print('\n\n\n')
     print("*****************************\n")
     print('RX/TX', rx_cpu, 'multi-streamer' if multi_streamer else 'single','rx_delay='+str(rx_delay),'\n')
@@ -39,7 +39,7 @@ def rx_benchmark(header='', duration=10, rx_rate=65e6, rx_channels="0,1,2,3", dr
 
 
 
-def tx_benchmark(header='', duration=10, tx_rate=65e6, tx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, tx_otw=True, sc='sc16', tx_cpu="sc16", tx_delay=5):
+def tx_benchmark(duration=10, tx_rate=65e6, tx_channels="0,1,2,3", drop_threshold=0, seq_threshold=0, multi_streamer=True, tx_otw=True, sc='sc16', tx_cpu="sc16", tx_delay=5):
     print('\n\n\n')
     print("*****************************\n")
     print('TX', tx_cpu, 'multi-streamer' if multi_streamer else 'single', 'tx_delay='+str(tx_delay),'\n')
@@ -95,19 +95,19 @@ def rx_tx_benchmark(duration=10, tx_rate=21666667, rx_rate=21666667, channels="0
 
 
 def main():
-    tx_benchmark(header='Tx, all channels, sc16')
-    tx_benchmark(header='Tx, channel 0, sc16', tx_channels="0")
-    tx_benchmark(header='Tx, channel 0&1, sc16', tx_channels="0,1")
-    tx_benchmark(header='Tx, all channels, fc32', tx_cpu='fc32')
+    tx_benchmark()
+    tx_benchmark(tx_channels="0")
+    tx_benchmark(tx_channels="0,1")
+    tx_benchmark(tx_cpu='fc32')
 
-    rx_benchmark(header='Rx, multi-streamer, sc16, no delay')
-    rx_benchmark(header='Rx, multi-streamer, sc16, 5s delay', rx_delay=5)
-    rx_benchmark(header='Rx, multi-streamer, fc32, 5s delay', rx_rate=21666667, rx_cpu='fc32', rx_delay=5)
-    rx_benchmark(header='Rx, single, fc32, 0s delay', rx_rate=21666667, rx_cpu='fc32', multi_streamer=False)
+    rx_benchmark()
+    rx_benchmark(rx_delay=5)
+    rx_benchmark(rx_rate=21666667, rx_cpu='fc32', rx_delay=5)
+    rx_benchmark(rx_rate=21666667, rx_cpu='fc32', multi_streamer=False)
 
-    rx_tx_benchmark(header="Rx/Tx sc16, single, with delay", multi_streamer=False)
-    rx_tx_benchmark(header="Rx/Tx sc16, multi, nodelay", tx_delay=0.5, rx_delay=0)
-    rx_tx_benchmark(header="Rx/Tx fc32, single, nodelay", tx_delay=0.5, rx_delay=0, multi_streamer=False, rx_cpu='fc32', tx_cpu="fc32")
+    rx_tx_benchmark(multi_streamer=False)
+    rx_tx_benchmark(tx_delay=0.5, rx_delay=0)
+    rx_tx_benchmark(tx_delay=0.5, rx_delay=0, multi_streamer=False, rx_cpu='fc32', tx_cpu="fc32")
 
 main()
 
