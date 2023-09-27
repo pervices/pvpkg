bc24f99e5f23d4424ff6e5d161c6c0795a34d1f9
Chen Jin <chen.j@pervices.com>
2023-09-27 14:59:54 +0000

    11423 RX CI TEST: fixed test_rx_stack_2 test and added to mandatory test list

--- Testing/tests/test_rx_stack_2.py
+++ Testing/tests/test_rx_stack_2.py
@@ -4,6 +4,7 @@ from gnuradio import gr
 
 from common import crimson
 import time
+import math
 import numpy as np
 import sys 
 
@@ -45,9 +46,11 @@ def main():
 
     # Enqueue one RX command every second starting at <start> and ending at <end> times.
     csrc.set_time_now(uhd.time_spec(0.0))
+    host_start_time = time.perf_counter()
     start = 10       #Start Time
     end = 15         #End Time
     interval = 1    #Increment time (Wait this long between subsequent Rx commands)
+    poll_delay = interval/2.0 # When to poll the data to see how much has been received
 
     #Create an expected Rx sample count array for the amount of samples expected to be received
     sample_count_array=np.arange(sample_count, int(sample_count*((end-start)+1)), sample_count, dtype=np.int32)
@@ -55,7 +58,6 @@ def main():
     expect_count_array= np.append(zero_count_array, sample_count_array)
     print("the theoretical expected sample count array is:", expect_count_array)
 
-    interval_additional_delay_coefficient = 0.1          # Time at which we poll between Rx commands
     for second in range(start, end, interval):
         cmd = uhd.stream_cmd_t(uhd.stream_mode_t.STREAM_MODE_NUM_SAMPS_AND_DONE)
         cmd.num_samps = sample_count
@@ -65,22 +67,32 @@ def main():
 
     #Poll for incoming RX commands and print the length of the vector sink.
 
-    ch_1_array = []
-    ch_2_array = []
-    ch_3_array = []
-    ch_4_array = []
-    for second in range(end):
-        #print(channel)
-        print("%d: %d: %d" % (0, second, len(vsnk[0].data())))
-        ch_1_array.append(len(vsnk[0].data()))
-        print("%d: %d: %d" % (1, second, len(vsnk[1].data())))
-        ch_2_array.append(len(vsnk[1].data()))
-        print("%d: %d: %d" % (2, second, len(vsnk[2].data())))
-        ch_3_array.append(len(vsnk[2].data()))
-        print("%d: %d: %d" % (3, second, len(vsnk[3].data())))
-        ch_4_array.append(len(vsnk[3].data()))
+    ch_1_array = [0] * end
+    ch_2_array = [0] * end
+    ch_3_array = [0] * end
+    ch_4_array = [0] * end
+
+    config_time = time.perf_counter() - host_start_time
+    sampling_start_time = math.ceil(config_time)
+    time.sleep(sampling_start_time - config_time)
+
+    for second in range(sampling_start_time, end):
+        start_time = time.perf_counter()
+        time.sleep(poll_delay)
+        
+        ch_1_array[second] = (len(vsnk[0].data()))
+        ch_2_array[second] = (len(vsnk[1].data()))
+        ch_3_array[second] = (len(vsnk[2].data()))
+        ch_4_array[second] = (len(vsnk[3].data()))
+
         #Populate slot 1 of that array with the sample count for that time interval
-        time.sleep(interval+interval*interval_additional_delay_coefficient)
+        print("%d: %d: %d" % (0, second, len(vsnk[0].data())))
+        print("%d: %d: %d" % (1, second, len(vsnk[1].data())))
+        print("%d: %d: %d" % (2, second, len(vsnk[2].data())))
+        print("%d: %d: %d" % (3, second, len(vsnk[3].data())))
+
+        while(time.perf_counter() - start_time < interval):
+            pass
 
     ch_1_actual_array=np.asarray(ch_1_array)
     ch_2_actual_array=np.asarray(ch_2_array)
