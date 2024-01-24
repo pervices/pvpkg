39ba6bdece79f328ba1ee0195d4014f55237375b
Isaac Ho <isaacho0722@gmail.com>
2024-01-24 15:57:25 -0500

    pdf report: changed testing code

--- Testing/tests/common/pdf_report.py
+++ Testing/tests/common/pdf_report.py
@@ -106,8 +106,13 @@ if __name__ == "__main__":
     report = ClassicShipTestReport("test_report")
 
     report.insert_text_large("Lorem Ipsum.")
-
-    for i in range(2):
-        report.insert_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    report.insert_text(" ")
+    report.insert_text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor")
+    report.insert_text("incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis")
+    report.insert_text("nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+    report.insert_text("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore")
+    report.insert_text("eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt")
+    report.insert_text("in culpa qui officia deserunt mollit anim id est laborum.")
+    report.insert_text(" ")
 
     report.save()
