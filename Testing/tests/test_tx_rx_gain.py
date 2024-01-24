d978708509cc12c03ec6d730a7df18d7579f2083
Isaac Ho <isaacho0722@gmail.com>
2024-01-24 12:50:14 -0500

    tx rx gain: adjust output pdf figure and text

--- Testing/tests/test_tx_rx_gain.py
+++ Testing/tests/test_tx_rx_gain.py
@@ -60,17 +60,17 @@ def main(iterations):
                     s = report.get_image_io_stream()
                     plt.savefig(s, format='png')
                     # plt.savefig(fname='Gain plot for channel {} at wave_freq {} at Tx gain {}'.format(ch, it["wave_freq"],it["tx_gain"],format='png'))
+                    report.insert_text("Gain plot of {} for wave_freq = {} Hz at Tx gain {} : ".format(ch,it["wave_freq"], it["tx_gain"]))
                     report.insert_image_from_io_stream(s)
-                    report.insert_text("Gain plot for channel at wave_freq at Tx gain")
-                    print("image should be inserted")
+                    print("image inserted for Gain plot of {} for wave_freq = {} Hz at Tx gain {}".format(ch,it["wave_freq"], it["tx_gain"]))
 
                     try:
                         assert iteration_areas[b+1][a] - iteration_areas[b][a] > 1 #makes sure the difference in area is significant
                     except:
                         print("insignificant difference in area")
-                        report.insert_text("Test failed, insignificant difference in area")
-                        report.save()
-                        sys.exit(1)
+                        report.insert_text("The above test failed, insignificant difference in area")
+                        # report.save()
+                        # sys.exit(1)
     report.save()
 
 
