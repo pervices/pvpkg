e3585c90ef7e721359a20d053982481dc50950ae
Victor Wollesen <victor.w@pervices.com>
2024-05-15 15:57:46 +0000

    PDF_REPORT: Fix filename.

--- Testing/tests/common/pdf_report.py
+++ Testing/tests/common/pdf_report.py
@@ -399,7 +399,7 @@ class ClassicShipTestReport:
         os.system('rm shiptest_out.txt')
 
         # Update doc_title, file_title, and file_name with the unit_name:
-        self.doc_title  = self.formattedDate + "-" + doc_title + "-" + unit_name + "-" + serial_num
+        self.doc_title  = self.doc_title + "-" + unit_name
         self.file_title = self.doc_title
         self.file_name =  self.output_dir + "/" + self.doc_title + ".pdf"
 
