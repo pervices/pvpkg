c85ca049a9d0ddd4029468a5c9039485d2a52914
Amelia MacLatchy <amelia.m@pervices.com>
2024-06-05 20:26:55 +0000

    TEMPORARY: Exempt tx_trigger test while Chen fixes issue

--- Testing/variables.sh
+++ Testing/variables.sh
@@ -30,7 +30,7 @@ WEBSITE_VERSION_LENGTH=40
 
 V_TEST_NAMES=("TX/RX Fundamental Frequency" "TX Trigger" "TX/RX Gain" "TX/RX Phase Short" "GPIO Stack Test" "RX Stack Test 1"  "RX Stack Test 2"  "TX Stack Test" "TX/RX Stacked Commands" "Tx/Rx Long Phase Coherency Test" "UHD Tx Manual Tuning Test" "UHD Rx Manual Tuning Test")
 V_TEST_FILES=(test_tx_rx_fundamental_frequency test_tx_trigger test_tx_rx_gain test_tx_rx_phase test_gpio_stack test_rx_stack test_rx_stack_2 test_tx_stack test_tx_rx_stacked_commands test_tx_rx_phase_2 test_tx_uhd_tuning test_rx_uhd_tuning)
-V_TEST_EXCEPTIONS=(false false false false false false false false true false false false)
+V_TEST_EXCEPTIONS=(false true false false false false false false true false false false)
 
 T_TEST_NAMES=("TX/RX Fundamental Frequency" "TX Trigger" "TX/RX Gain" "TX/RX Phase Short" "GPIO Stack Test" "RX Stack Test 1"  "RX Stack Test 2"  "TX Stack Test" "TX/RX Stacked Commands" "Tx/Rx Long Phase Coherency Test" "UHD Tx Manual Tuning Test" "UHD Rx Manual Tuning Test")
 T_TEST_FILES=(test_tx_rx_fundamental_frequency test_tx_trigger test_tx_rx_gain test_tx_rx_phase test_gpio_stack test_rx_stack test_rx_stack_2 test_tx_stack test_tx_rx_stacked_commands test_tx_rx_phase_2 test_tx_uhd_tuning test_rx_uhd_tuning)
