b0bf75c356b7890a343f3a35a6cace3156f0b498
Amelia MacLatchy <amelia.m@pervices.com>
2024-08-28 18:31:56 +0000

    TEST,GAIN: Fix channel plotting and tracking

--- Testing/tests/test_tx_rx_gain.py
+++ Testing/tests/test_tx_rx_gain.py
@@ -9,12 +9,6 @@ import sys
 import os
 
 def main(iterations, title="TX RX Gain Test") -> int:
-
-    # Collect.
-    vsnks = []
-
-    sample_count = 0
-
     fail_flag = 0
 
     for it in iterations:
@@ -29,82 +23,72 @@ def main(iterations, title="TX RX Gain Test") -> int:
             report.save()
             sys.exit(1)
 
-        vsnks.append(vsnk)
-        # print("new iteration")
-        # print(len(vsnks))
-
         iteration_areas = []
-        current_vsnk_i = 0
         current_test_only_fail_flag = 0
 
-        for vsnk in vsnks:
-            channel_areas = []
-            images = []
-            current_vsnk_i += 1
-            # print("vsnk")
-            for ch, channel in enumerate(vsnk):
-                # print("channel")
-                real = [datum.real for datum in channel.data()]
-                imag = [datum.imag for datum in channel.data()]
-                #print('the value of the real array is', real)
-                #print('the value of the imag array is', imag)
+        channel_areas = []
+        images = []
+        for ch, channel in enumerate(vsnk):
+            # print("channel")
+            real = [datum.real for datum in channel.data()]
+            imag = [datum.imag for datum in channel.data()]
+            #print('the value of the real array is', real)
+            #print('the value of the imag array is', imag)
 
-                # Discard the first 50 unstable samples
-                real = real[50:]
-                imag = imag[50:]
+            # Discard the first 50 unstable samples
+            real = real[50:]
+            imag = imag[50:]
 
-                ## Calculate absolute area.
-                area = sigproc.absolute_area(real)
-                channel_areas.append(area)
+            ## Calculate absolute area.
+            area = sigproc.absolute_area(real)
+            channel_areas.append(area)
 
-                #plot and save real component
-                plt.figure()
-                plt.title("Gain plot of ch{} for wave_freq = {} Hz".format(ch,it["wave_freq"]))
-                plt.xlabel("Sample")
-                plt.ylabel("Amplitude")
-                plt.plot(imag[0:300], label='reals')
-                plt.plot(real[0:300], label='imags')
-                plt.legend()
+            #plot and save real component
+            plt.figure()
+            plt.title("Gain plot of ch{} for wave_freq = {} Hz".format(ch,it["wave_freq"]))
+            plt.xlabel("Sample")
+            plt.ylabel("Amplitude")
+            plt.plot(imag[0:300], label='reals')
+            plt.plot(real[0:300], label='imags')
+            plt.legend()
 
-                s = report.get_image_io_stream()
-                plt.savefig(s, format='png', dpi=200)
-                plt.close()     # remember to close or you'll use up all the memory
-                img = report.get_image_from_io_stream(s)
-                images.append(img)
+            s = report.get_image_io_stream()
+            plt.savefig(s, format='png', dpi=200)
+            plt.close()     # remember to close or you'll use up all the memory
+            img = report.get_image_from_io_stream(s)
+            images.append(img)
 
 
-            iteration_areas.append(channel_areas)
-            #areas = np.array(areas).T.tolist() # Transpose.
-            print("the areas of channel 0-3 for gain [5,10,20] are:", iteration_areas)
-            # Assert area is increasing per channel.
-            desc = "Gain plot of channel {} for wave_freq = {} Hz at Tx gain {} and Rx gain {} : ".format(it["channels"], it["wave_freq"], it["tx_gain"], it["rx_gain"])
-            data = [["Center Frequency (Hz)", "Wave Frequency (Hz)", "Sample Rate (SPS)", "Sample Count", "TX Gain (dB)", "RX Gain (dB)"],
+        iteration_areas.append(channel_areas)
+        #areas = np.array(areas).T.tolist() # Transpose.
+        print("the areas of channel 0-3 for gain [5,10,20] are:", iteration_areas)
+        # Assert area is increasing per channel.
+        desc = "Gain plot of channel {} for wave_freq = {} Hz at Tx gain {} and Rx gain {} : ".format(it["channels"], it["wave_freq"], it["tx_gain"], it["rx_gain"])
+        data = [["Center Frequency (Hz)", "Wave Frequency (Hz)", "Sample Rate (SPS)", "Sample Count", "TX Gain (dB)", "RX Gain (dB)"],
                         [it["center_freq"], it["wave_freq"], it["sample_rate"], it["sample_count"], it["tx_gain"], it["rx_gain"]]]
 
 
-            for a in range(len(iteration_areas[0])):
-                #print(area)
-                for b in range(len(iteration_areas)-1):
-                    # test for area
-                    try:
-                        # make sure the difference in area is significant
-                        assert iteration_areas[b+1][a] - iteration_areas[b][a] > 1
-                    except:
-                        print(sys.argv[0] + " unacceptable variation in gain")
-                        fail_flag = 1
-                        current_test_only_fail_flag = 1
+        for a in range(len(iteration_areas[0])):
+            #print(area)
+            for b in range(len(iteration_areas)-1):
+                # test for area
+                try:
+                    # make sure the difference in area is significant
+                    assert iteration_areas[b+1][a] - iteration_areas[b][a] > 1
+                except:
+                    print(sys.argv[0] + " unacceptable variation in gain")
+                    fail_flag = 1
+                    current_test_only_fail_flag = 1
 
 
-            if (current_vsnk_i == (len(vsnks) - 1) or len(vsnks) == 1):
-                # dont draw unnecessary stuff
-                report.buffer_put("pagebreak")
-                report.buffer_put("text_large", title)
-                report.buffer_put("table_wide", data, "Test Configuration")
-                report.buffer_put("text", " ")
-                report.buffer_put("image_quad", images, desc)
-                if (current_test_only_fail_flag == 1):
-                    report.buffer_put("text_large", "This test has failed")
-                    current_test_only_fail_flag = 0
+        report.buffer_put("pagebreak")
+        report.buffer_put("text_large", title)
+        report.buffer_put("table_wide", data, "Test Configuration")
+        report.buffer_put("text", " ")
+        report.buffer_put("image_quad", images, desc)
+        if (current_test_only_fail_flag == 1):
+            report.buffer_put("text_large", "This test has failed")
+            current_test_only_fail_flag = 0
 
     # report.save()
     if (fail_flag == 1):
