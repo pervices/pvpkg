87feab64d6f0e520f0127bf50367c34294be5f4e
Doug W. <doug.w@pervices.com>
2024-10-23 19:12:10 +0000

    TEST_TX_RX_RATE: fixing exiting with 0 instead of actual error code

--- Testing/tests/test_tx_rx_rate.py
+++ Testing/tests/test_tx_rx_rate.py
@@ -41,8 +41,9 @@ def test(it):
     else:
         iteration_result = os.system("/usr/lib/uhd/examples/benchmark_rate --priority high --tx_rate={} --tx_channels {}  --overrun-threshold 0 --underrun-threshold 0 --drop-threshold 0 --seq-threshold 0".format(it["tx_rate"], list_to_arg_string(it["tx_channel"])))
 
-    # Update flag if the test failed
-    test_fail = test_fail | iteration_result
+    # Set test_fail to the return code of the first failed iteration
+    if(iteration_result and not test_fail):
+        test_fail = iteration_result
 
     result_string = "Fail" if iteration_result != 0 else "Pass"
 
@@ -80,5 +81,8 @@ else:
     print("Error: invalid product specified")
 
 build_report()
+# sys.exit only takes values in range 0-255 benchmark_rate returns EXIT_FAILURE which may be > 255. If that happens replace the value with 1
+if(test_fail > 255):
+    test_fail = 1
 sys.exit(test_fail)
 
