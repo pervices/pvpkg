5c26c5ac651cc0321fedbe208081fb71b337ca0d
Amelia MacLatchy <amelia.m@pervices.com>
2024-11-21 21:21:56 +0000

    CYAN,GEN: Reduce uhd tuning test gains
    Reduce gain so that the main tone does not drown out the LO feedthrough
    that the test is searching for

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -490,8 +490,8 @@ class cyan:
             channels = list(range(4))
             wave_freq = 1000000
             sample_count = 10000
-            tx_gain = 15            # keep gains low so strength of tone doesn't drown out LO feedthrough
-            rx_gain = 15
+            tx_gain = 0            # keep gains low so strength of tone doesn't drown out LO feedthrough
+            rx_gain = 0
             rx_lo = 2300000000 #LO Frequency should be multiple of 100MHz for cyan/chestnut
             sample_rate = 10000000
             for center_freq in [ (rx_lo - 2000000), rx_lo, (rx_lo + 2000000) ]: # 3 cases for dsp nco (pos, zero, neg).
@@ -503,8 +503,8 @@ class cyan:
             channels = list(range(4))
             wave_freq = 1000000
             sample_count = 10000
-            tx_gain = 15            # keep gains low so strength of tone doesn't drown out LO feedthrough
-            rx_gain = 15
+            tx_gain = 0            # keep gains low so strength of tone doesn't drown out LO feedthrough
+            rx_gain = 0
             tx_lo = 2300000000 #LO Frequency should be multiple of 100MHz for cyan/chestnut
             sample_rate = 10000000
             for center_freq in [ (tx_lo - 2000000), tx_lo, (tx_lo + 2000000) ]: # 3 cases for dsp nco (pos, zero, neg).
