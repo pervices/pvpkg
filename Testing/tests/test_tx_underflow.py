ebb4507ab886011bdf859f4b85f2f23f9e4042f9
John Polak <john.p@pervices.com>
2024-12-13 22:32:32 +0000

    python whitespace BS fixing unexpected indent

--- Testing/tests/test_tx_underflow.py
+++ Testing/tests/test_tx_underflow.py
@@ -26,7 +26,7 @@ def test(it):
         sys.exit(test_fail)
 
     uhd_cmd = subprocess.Popen(["/usr/lib/uhd/examples/tx_waveforms", "--rate", str(it["sample_rate"]), "--freq", str(it["center_freq"]), "--gain", str(it["tx_gain"])], stdout=subprocess.PIPE)
-'''
+    '''
     # Read from STDOUT until we see that the Actual TX Rate has been set
     for line in uhd_cmd.stdout:
         print("STDOUT: ", line)
@@ -35,7 +35,7 @@ def test(it):
 
     # set the SDR's rate to 10x the rate UHD is expecting. This should case underflow
     subprocess.run(["/usr/bin/uhd_manual_set", "--path", "/mboards/0/tx_dsps/0/rate/value", "--value", str(it["sample_rate"]), "--type", "double"])
-'''
+    '''
     time.sleep(20)
     uhd_cmd.send_signal(signal.SIGINT)
 
