4baadc351bf90395bb3a8caca499f6a691054018
John Polak <john.p@pervices.com>
2024-12-13 22:00:00 +0000

    fix typon in path of uhd_manual_set

--- Testing/tests/test_tx_underflow.py
+++ Testing/tests/test_tx_underflow.py
@@ -32,7 +32,7 @@ def test(it):
             break
 
     # set the SDR's rate to 10x the rate UHD is expecting. This should case underflow
-    subprocess.run(["/usr/lib/uhd_manual_set", "--path", "/mboards/0/tx_dsps/0/rate/value", "--value", str(it["sample_rate"]*10), "--type", "double"])
+    subprocess.run(["/usr/bin/uhd_manual_set", "--path", "/mboards/0/tx_dsps/0/rate/value", "--value", str(it["sample_rate"]*10), "--type", "double"])
 
     uhd_cmd.communicate() # Block Python until uhd_cmd Popen process exits
     print("making sure that I waited")
