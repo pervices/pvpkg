a8485ecd3e950a187607476eea7a36541680101b
Amelia MacLatchy <amelia.m@pervices.com>
2025-01-16 20:34:05 +0000

    TEST_ARGS: Add channel list to test arguments

--- Testing/tests/common/test_args.py
+++ Testing/tests/common/test_args.py
@@ -4,6 +4,7 @@ import sys
 class TestArgs:
     serial = None
     product = None
+    channels = None
     report_dir = None
     docker_sha = None
 
@@ -12,10 +13,20 @@ class TestArgs:
             parser = argparse.ArgumentParser(description = testDesc)
         parser.add_argument('-s', '--serial', required=True, default=None, help="Serial number of the unit")
         parser.add_argument('-p', '--product', required=True, help="Product, v for vaunt t for tate l for lily")
+        parser.add_argument('-c', '--channels', required=False, default=[0,1,2,3], help="Channel list to use for testing. Example usage: [0,1,2,3]")
         parser.add_argument('-o', '--output', required=False, default=None, help="Report output directory")
         parser.add_argument('-d', '--docker', required=False, default=None, help="Docker SHA")
         args = parser.parse_args()
+        
         self.serial = args.serial
+        if args.channels.len() > 8 or args.channels.len() < 1:
+            print("[ERROR][{}][{}]: Channels list must contain between 1 and 8 channels".format(frameinfo.filename, frameinfo.lineno))
+            sys.exit(1)
+        if len(a) != len(set(a)):
+            print("[ERROR][{}][{}]: Channels list must contain unique elements.".format(frameinfo.filename, frameinfo.lineno))
+        else:
+            self.channels = args.channels
+        
         if args.product == 'v':
             self.product = "Vaunt"
         elif args.product == 't':
