927fbbfdcbe5fc087fd865f8c14c4082d372ccb5
Chen Jin <chen.j@pervices.com>
2025-01-20 09:48:55 -0500

    DEBUG: increaseing the wave frequency

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -119,12 +119,12 @@ def lo_band_basic():
     print(sys._getframe().f_code.co_name)
 
     channels = list(range(4))
-    wave_freq = 1000000
+    wave_freq = 16000000
     sample_count = 256
     tx_gain = 25
     rx_gain = 25
-    for center_freq in [ 15000000 ]:
-        for sample_rate in [ 9848485, 25000000 ]:
+    for center_freq in [ 0 ]:
+        for sample_rate in [ 25000000 ]:
             yield locals()
 
 
