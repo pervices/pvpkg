b519c312db6f9a7d8b30971052be4ef52071240f
Victor Wollesen <victor.w@pervices.com>
2025-02-12 20:25:48 +0000

    OpenPorts-configSFP.sh: Improve help message and symantics.

--- Oracle/Oracle8/openports-configSFP.sh
+++ Oracle/Oracle8/openports-configSFP.sh
@@ -2,42 +2,132 @@
 
 #This is a script to configure and update the firewall on redhat.
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
+#TODO: Ensure this command works safely, with the following
+
+set -Eeuo pipefail
+
+# #Set flags
+# # -e : Exit immediately on command failure
+# # -o pipefail : propagate exit codes on pipes to right most.
+# # -u : treat unset variables as an error
+# # -x : print each command prior to executing it.
+# # -E : ensure that errors are caught and cleaned up.
+
+# trap "echo Trap was triggered" cleanup SIGINT SIGTERM ERR EXIT
+
+#Synopsis Help:
+
+function help_synopsis {
+    echo -e "SYNOPSIS"
+    echo -e "\t $0 [ help | <iface_name> <cyan_qSFP_nbr> ]"
+    echo -e "Where:"
+    echo -e "\t iface_name"
+    echo -e "\t\t Interface name to configure."
+    echo -e "\t cyan_qSFP_nbr [0|1|2|3]"
+    echo -e "\t\t Cyan qSFP port number connected to <iface_name>,"
+    echo -e "\t\t with Cyan qSFP+ port A corresponding to 0, Cyan qSFP+ D to 4."
+    echo -e ""
+    echo -e "For detailed help and example invocations, run:"
+    echo -e "\t $0 help"
+    exit 1
+}
+
+#Detailed Help;
+function help_detailed {
+    echo -e "USAGE"
+    echo -e "\t $0 help"
+    echo -e "\t\t Display this detailed help message."
+    echo -e ""
+    echo -e "\t $0 <InterfaceName> <ChannelNumber>"
+    echo -e "\t\t Configure interface for use with Cyan qSFP+ channel number"
+    echo -e "Where:\n"
+    echo -e "<InterfaceName>"
+    echo -e "\t Device name of the network interface you wish to configure."
+    echo -e "\t To list all available interfaces, run:"
+    echo -e "\t\t ip link show"
+    echo -e ""
+    echo -e "<ChannelNumber> [0|1|2|3]"
+    echo -e "\t The number corresponding to the Cyan SFP+ port connected to"
+    echo -e "\t the specified <InterfaceName>, where:"
+    echo -e "\t\t Cyan qSFP+ A = 0"
+    echo -e "\t\t Cyan qSFP+ B = 1"
+    echo -e "\t\t Cyan qSFP+ C = 2"
+    echo -e "\t\t Cyan qSFP+ D = 3"
+    echo -e ""
+    echo -e "EXAMPLES:"
+    echo -e "\t After physically connecting the interface \"enp1s0f1\" to the"
+    echo -e "\t Cyan qSFP+ port A, run:"
+    echo -e "\t\t  $0 enp1s0f1 0"
+    echo -e "\t After physically connecting the interface \"enp0s31f6\" to the"
+    echo -e "\t Cyan qSFP+ port D, run:"
+    echo -e "\t\t  $0 enp0s31f6 3"
+    exit 1
+}
+
+
+# If the first argument is help, display detailed help:
+if [ "$#" -eq 1 ] && [ "$1" == 'help' ]; then
+    help_detailed;
 fi
 
+# Display an error if run with incorrect number of arguments:
+if [ "$#" -ne 2 ]; then
+    echo "ERROR: Invalid Number of Arguments"
+    help_synopsis;
+fi
+
+##
+# Runtime environment checks
+##
+
+# Program requires root privileges to configure interfaces and firewall.
+if [ "$EUID" -ne 0 ]; then
+    echo "ERROR: Please run this program as the root user."
+    exit 1
+fi
+
+
+##
+# Parse arguments
+##
+
+IFACE_NAME=$1
+CYAN_PORT=$2
+
+##
+# Core program arguments
+##
+
+CMD_IP=ip
+CMD_FW=firewall-cmd
+
+#DEBUG:
+#CMD_IP="echo ip"
+#CMD_FW="echo firewall-cmd"
+
 if [ "$#" -eq 2 ]; then
-# $1 is the network interface and $2 is the number
+    echo -e "\nConfigure interface IP address and MTU."
+            $CMD_IP addr add 10.10.1$CYAN_PORT.10/24 broadcast + dev $IFACE_NAME
+            $CMD_IP link set mtu 9000 dev $IFACE_NAME
 
-ports='42836 42837 42838 42839 42840 42841 42842 42843 42809 42810 42811 42812 42799'
+    # List of port numbers to be opened:
+    ports='42836 42837 42838 42839 42840 42841 42842 42843 42809 42810 42811 42812 42799'
+    echo -e "\nOpening firewalld ports:"
+        for port in $ports
+        do
+            $CMD_FW --zone=trusted --permanent --add-port=$port/udp
+        done
 
-echo -e "Configuring qSFP+ NIC\n"
+    echo -e "\nAdding nics to trusted zones:"
+        #Add adapter to trusted
+        $CMD_FW --zone=trusted --permanent --change-interface=$IFACE_NAME
+        #Add SDR IP address to trusted
+        $CMD_FW --zone=trusted --permanent --add-source=10.10.1$CYAN_PORT.2
+        $CMD_FW --reload
 
-	ip addr add 10.10.1$2.10/24 broadcast + dev $1
-	ip link set mtu 9000 dev $1
-
-echo -e "Opening firewalld ports.\n"
-
-for port in $ports
-do
-	firewall-cmd --zone=trusted --permanent --add-port=$port/udp
-done
-
-echo -e "Adding nics to trusted zones.\n"
-
-	#Add adapter to trusted
-	firewall-cmd --zone=trusted --permanent --change-interface=$1
-	#Add SDR IP address to trusted
-	firewall-cmd --zone=trusted --permanent --add-source=10.10.1$2.2
-
-	firewall-cmd --reload
-
-exit 0
-
+    echo -e "Finished.\n"
+    exit 0
 else
-    echo "Usage: ./openports-configSFP.sh {network interface} {number}"
-    echo "For example, to use enp1s0 with the IP address 10.10.10.10 (corresponding to SPFA):"
-    echo "./openports-configSFP.sh enp1s0 0"
+    help_detailed;
     exit 1
 fi
