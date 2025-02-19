eb1013bd7ba98586da1161f9cd5c124ee3279157
Chen Jin <chen.j@pervices.com>
2025-02-19 09:34:39 -0500

    Restored gain to 25

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -317,8 +317,8 @@ class cyan:
             channels = list(range(4))
             wave_freq = 2500000         # 1MHz
             sample_count = 256
-            tx_gain = 10
-            rx_gain = 10
+            tx_gain = 25
+            rx_gain = 25
             for center_freq in [ 100000000, 0 ]:
                 for sample_rate in [ 25000000, 50000000 ]:
                     yield locals()
