2088cba5f63c7999c719f53069de6b7455cbca60
Doug W. <doug.w@pervices.com>
2025-06-25 18:06:19 +0000

    TEST_TX_RX_RATE: lower Cyan/Chestnut targets
    We have never reached them reliably inside docker. I lowered the
    expectations so the test can be used to detect regressions, instead of
    always being excempt.

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -365,11 +365,10 @@ class cyan:
             print(sys._getframe().f_code.co_name)
 
             descriptions = ["Max achievable rx rate on any number of ch", "Max achievable tx rate on any number of ch", "Max achievable combined rate on all ch", "Max achievable rx rate on all ch", "Max achievable tx rate on all ch"]
-            # TODO: 1Gsps should be achievable but currently there is an issue causing random latency spikes, once that is fixed increase single channel rate from 500Msps to 1Gsps
-            # 11-13-2024: 250Msps on all channels works perfectly on Maple
-            rx_rates = [500e6, 0, 250e6, 250e6, 250e6]
+            # Higher rates are achievable outside of docker
+            rx_rates = [250e6, 0, 100e6, 125e6, 0]
             rx_channels = [[0], [], list(range(channels)), list(range(channels)), []]
-            tx_rates = [0, 500e6, 250e6, 0, 250e6]
+            tx_rates = [0, 250e6, (500e6)/8, 0, (500e6)/8]
             tx_channels = [[], [0], list(range(channels)), [], list(range(channels))]
             assert(len(rx_rates) == len(rx_channels))
             assert(len(rx_rates) == len(tx_rates))
@@ -636,10 +635,11 @@ class chestnut:
         def tx_rx_rate(channels):
             print(sys._getframe().f_code.co_name)
 
-            descriptions = ["Max achievable rx rate on any number of ch", "Max achievable tx rate on any number of ch", "Max achievable combined rate on all ch", "Max achievable rx rate on all ch", "Max achievable tx rate on all ch"]
-            rx_rates = [500e6, 0, 250e6, 250e6, 250e6]
+            descriptions = ["Max achievable rx rate on one ch", "Max achievable tx rate on one ch", "Max achievable combined rate on all ch", "Max achievable rx rate on all ch", "Max achievable tx rate on all ch"]
+            # Higher rates are achievable outside of docker
+            rx_rates = [250e6, 0, 100e6, 125e6, 0]
             rx_channels = [[0], [], list(range(channels)), list(range(channels)), []]
-            tx_rates = [0, 500e6, 250e6, 0, 250e6]
+            tx_rates = [0, 250e6, (500e6)/8, 0, (500e6)/8]
             tx_channels = [[], [0], list(range(channels)), [], list(range(channels))]
             assert(len(rx_rates) == len(rx_channels))
             assert(len(rx_rates) == len(tx_rates))
@@ -723,11 +723,11 @@ class chestnut:
         def tx_rx_rate(channels):
             print(sys._getframe().f_code.co_name)
 
-            descriptions = ["Max achievable rx rate on any number of ch", "Max achievable tx rate on any number of ch", "Max achievable combined rate on all ch", "Max achievable rx rate on all ch", "Max achievable tx rate on all ch"]
-            # TODO: verify if these rates are achievable on the hosts used by CI
-            rx_rates = [500e6, 0, 125e6, 250e6, 0]
+            descriptions = ["Max achievable rx rate on one ch", "Max achievable tx rate on one ch", "Max achievable combined rate on all ch", "Max achievable rx rate on all ch", "Max achievable tx rate on all ch"]
+            rx_rates = [250e6, 0, 100e6, 125e6, 0]
             rx_channels = [[0], [], list(range(channels)), list(range(channels)), []]
-            tx_rates = [0, 500e6, 125e6, 0, 250e6]
+            # Higher speeds can be aheived when running natively but not through docker where this test is run
+            tx_rates = [0, 500e6, (500e6)/6, 0, (500e6)/6]
             tx_channels = [[], [0], list(range(channels)), [], list(range(channels))]
             assert(len(rx_rates) == len(rx_channels))
             assert(len(rx_rates) == len(tx_rates))
