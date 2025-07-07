44e652977ce8205c9da34f26c0d2d6b8dc319a11
Doug W. <doug.w@pervices.com>
2025-07-07 18:13:07 +0000

    CHESTNUT: adding tx_uhd_tune config

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -830,6 +830,21 @@ class chestnut:
             for center_freq in [ (rx_lo - 2000000), rx_lo, (rx_lo + 2000000) ]: # 3 cases for dsp nco (pos, zero, neg).
                 yield locals()
 
+        @staticmethod
+        def tx_uhd_tune():
+            print(sys._getframe().f_code.co_name)
+            channels = list(range(4))
+            wave_freq = 1000000
+            sample_count = 10000
+            # Using to low a gain will result in the lo feedthrough not being visible next to the main tone
+            # Using to high a gain will result in either the lo feedthrough or main tone not being visible
+            tx_gain = 0
+            rx_gain = 0
+            tx_lo = 2300000000 #LO Frequency should be multiple of 100MHz for cyan/chestnut
+            sample_rate = 10000000
+            for center_freq in [ (tx_lo - 2000000), tx_lo, (tx_lo + 2000000) ]: # 3 cases for dsp nco (pos, zero, neg).
+                yield locals()
+
     class hi_band:
         @staticmethod
         def wave_sweep():
