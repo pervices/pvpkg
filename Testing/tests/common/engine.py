b87fdb55f40960558e4cd5100339f3a48c4d9dbe
Doug W. <doug.w@pervices.com>
2025-07-08 22:26:42 +0000

    debug msg

--- Testing/tests/common/engine.py
+++ Testing/tests/common/engine.py
@@ -189,6 +189,10 @@ def manual_tune_run(channels, wave_freq, tx_sample_rate, rx_sample_rate, tx_tune
 
     # Prepare thread
     # Expected tx duration = start time of last burst + (length of last burst / sample rate)
+    tx_duration = tx_stack[-1][0]
+    print("T10B")
+    tx_duration = tx_stack[-1][0] + (tx_stack[-1][1] / 1)
+    print("T10C")
     tx_duration = tx_stack[-1][0] + (tx_stack[-1][1] / sample_rate)
     print("T11")
     tx_thread = threading.Thread(target = run_tx, args = (csnk, channels, tx_stack, tx_sample_rate, wave_freq))
