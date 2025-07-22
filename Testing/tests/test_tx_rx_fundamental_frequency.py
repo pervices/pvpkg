7306b87d94d5aafd5fdafa436fc554a3f078ba68
Joseph Talon <joseph.t@pervices.com>
2025-07-22 20:29:14 +0000

    TESTS,STACKED/FUNDAMENTAL: Fail test on engine exception and add column to report for number of attempts

--- Testing/tests/test_tx_rx_fundamental_frequency.py
+++ Testing/tests/test_tx_rx_fundamental_frequency.py
@@ -15,10 +15,15 @@ targs = test_args.TestArgs(testDesc="Tx Rx Fundamental Frequency Test")
 report = pdf_report.ClassicShipTestReport("tx_rx_fundamental_frequency", targs.serial, targs.report_dir, targs.docker_sha)
 test_fail = 0
 summary_tables = []
+max_attempts = 1
+attempt_num = 0
 
 @retry(stop_max_attempt_number = 1)
 def test(it, data):
     global test_fail
+    global attempt_num
+    attempt_num += 1
+    test_dnf = False
     gen.dump(it)
 
 
@@ -27,16 +32,29 @@ def test(it, data):
     try:
         vsnk = engine.run(targs.channels, it["wave_freq"], it["sample_rate"], it["center_freq"], it["tx_gain"], it["rx_gain"], tx_stack, rx_stack)
     except Exception as err:
-        build_report()
-        sys.exit(1)
+        test_fail = 1
+        if attempt_num < max_attempts:
+            raise
+        else:
+            test_dnf = True
 
     center_freq = "{:.1e}".format(it["center_freq"])
     wave_freq = "{:.1e}".format(it["wave_freq"])
     title = "Center freq: {}, Wave freq: {}".format(center_freq, wave_freq)
-    test_info = [["Center Frequency (Hz)", "Wave Frequency (Hz)", "Sample Rate (SPS)", "Sample Count", "TX Gain (dB)", "RX Gain (dB)"],
-                        [center_freq, wave_freq, it["sample_rate"], it["sample_count"], it["tx_gain"], it["rx_gain"]]]
+    test_info = [["Center Frequency (Hz)", "Wave Frequency (Hz)", "Sample Rate (SPS)", "Sample Count", "TX Gain (dB)", "RX Gain (dB)", "Attempts"],
+                        [center_freq, wave_freq, it["sample_rate"], it["sample_count"], it["tx_gain"], it["rx_gain"], attempt_num]]
 
+    report.buffer_put("text_large", title)
+    report.buffer_put("table_large", test_info, "")
+    report.buffer_put("text", " ")
     images = []
+
+    # Since engine.run failed, exit test early with DNF for missing data
+    if attempt_num >= max_attempts and test_dnf:
+        data.append([str(center_freq), str(wave_freq), "DNF", "fail"])
+        report.buffer_put("pagebreak")
+        return data
+
     for ch, channel in enumerate(vsnk):
         real = [datum.real for datum in channel.data()]
         imag = [datum.imag for datum in channel.data()]
@@ -63,25 +81,24 @@ def test(it, data):
         images.append(img)
 
         res = ""
-        if(like_real > 0.95 and like_real < 1.05 and like_imag > 0.95 and like_imag < 1.05):
+        if(like_real > 0.95 and like_real < 1.05 and like_imag > 0.95 and like_imag < 1.05 and not test_dnf):
             res = "pass"
         else:
             res = "fail"
             test_fail = 1
 
-        data.append([str(center_freq), str(wave_freq), str(ch), res])
+        data.append([str(center_freq), str(wave_freq), str(ch), attempt_num, res])
 
-    report.buffer_put("text_large", title)
-    report.buffer_put("table_wide", test_info, "")
-    report.buffer_put("text", " ")
     report.buffer_put("image_list_dynamic", images, "")
     report.buffer_put("pagebreak")
 
     return data
 
 def main(iterations, desc):
-    data  = [["Centre Freq", "Wave Freq", "Channel", "Result"]]
+    global attempt_num
+    data  = [["Centre Freq", "Wave Freq", "Channel", "Attempts", "Result"]]
     for it in iterations:
+        attempt_num = 0
         test(it, data)
     summary_tables.append([desc, data])
 
