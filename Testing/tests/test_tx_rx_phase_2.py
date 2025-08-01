4066283b1859ca1d1de4c11b8e0a0b3b48da62ed
Chen Jin <chen.j@pervices.com>
2025-08-01 10:45:41 -0400

    Change tx burst time

--- Testing/tests/test_tx_rx_phase_2.py
+++ Testing/tests/test_tx_rx_phase_2.py
@@ -29,7 +29,7 @@ import argparse
 #USER CHOSEN VALUES
 num_output_waves =1 #depends what plots look like
 begin_cutoff_waves = 1 #0.00000425 #e(-5) - guessed from previous diagrams (but seconds)
-tx_burst = 5.0 #burst should be slightly delayed to ensure all data is being collected
+tx_burst = 5.20 #burst should be slightly delayed to ensure all data is being collected
 rx_burst = 5.25
 
 std_ratio = 4  #number std gets multiplied by for checks, normalized to a sample size of 10
