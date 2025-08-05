82e5535b22f4cfa553f634069fa494d85c88de64
Joseph Talon <joseph.t@pervices.com>
2025-08-05 14:53:31 +0000

    PDF_REPORT: Update cmp_time regex split

--- Testing/tests/common/pdf_report.py
+++ Testing/tests/common/pdf_report.py
@@ -460,7 +460,7 @@ class ClassicShipTestReport:
         pvpkg_branch = subprocess.run(["git rev-parse --abbrev-ref HEAD | tr -d '\n' "], shell=True, capture_output=True, text=True).stdout
         fpga_ddr = subprocess.run(["uhd_usrp_info --all | grep DDR | tr -d '\n' "], shell=True, capture_output=True, text=True).stdout
         cmp_time = subprocess.run(["uhd_manual_get --path /mboards/0/cmp_time | grep -Eo [0-9]+-[0-9]+-[0-9]+[[:blank:]][0-9]+:[0-9]+ | tr -d '\n' "], shell=True, capture_output=True, text=True).stdout
-        cmp_time_parts = list(map(int, re.split('-|:| ', cmp_time)))
+        cmp_time_parts = list(map(int, re.split('[-|:| ]', cmp_time)))
 
         os.system('rm shiptest_out.txt')
 
