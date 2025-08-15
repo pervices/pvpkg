69d192be5b426a9ab102d121602628656f8a3d03
Joseph Talon <joseph.t@pervices.com>
2025-08-15 15:29:27 +0000

    APT_CACHER_SERVER: Added script to start apt-cacher-ng Docker container

---
+++ common/common-conf/lib/apt-cacher-server/apt-cacher-server.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+set -Eeuo pipefail
+
+# DIAGNOSTICS
+SCRIPT_USER=`whoami`
+SCRIPT_HM=`hostname`
+SCRIPT_DATE=`date +%Y%M%dT%H%M%S.%N`
+SCRIPT_DIR=`pwd`
+SCRIPT_PROG=$0
+SCRIPT_ARGS=$@
+SCRIPT_DIAGNOSTICS_SHORT="$SCRIPT_USER@$SCRIPT_HM:$SCRIPT_DIR/$SCRIPT_PROG $SCRIPT_ARGS" 
+SCRIPT_UNAME=`uname -n -s -r -m`
+rc=0
+
+function print_diagnostics_short(){
+    echo "INFO: `date +%Y%M%dT%H%M%S.%N`: $SCRIPT_DIAGNOSTICS_SHORT"
+}
+
+function print_diagnostics() {
+    echo "----------------------------------------"
+    echo $0 $@
+    echo "----------------------------------------"
+    echo "Running as: $SCRIPT_USER@$SCRIPT_HM    "
+    echo "Date: $SCRIPT_DATE"
+    echo "Directory: $SCRIPT_DIR"
+    echo "Uname: $SCRIPT_UNAME"
+    echo "----------------------------------------"
+    echo "Current Time: `date +%Y%M%dT%H%M%S.%N`  "
+    echo "PWD: `pwd`                              "
+    echo "----------------------------------------"
+}
+
+function check_rc() {
+    local rc=$1
+    local cmd_name=${2:-"Previous command"}
+    if [[ $rc != 0 ]]; then
+        echo "----------------------------------------" 
+        echo "ERROR: ${cmd_name} returned non-zero exit code, exiting..." 
+        echo "----------------------------------------" 
+        print_diagnostics
+        exit $rc
+    fi
+}
+
+# Ensure the container name was provided as argument and assign to variable
+if [ -z $1 ]; then
+    echo "ERROR: Container name not provided as argument, exiting..."
+    print_diagnostics
+    exit 1
+fi
+CONTAINER_NAME=${1:-} # Container named after service
+CONTAINER_IMAGE="pvtesting/ubuntu2404:apt-cacher-ng"
+
+# Create container if it does not exist
+if [[ $(docker container inspect $CONTAINER_NAME) -gt 0 ]]; then
+    docker run --rm \
+    --detach \
+    --name $CONTAINER_NAME \
+    --network host \
+   $CONTAINER_IMAGE /usr/sbin/apt-cacher-ng ForeGround=1
+else
+    docker container start $CONTAINER_NAME || rc=$?
+    docker container attach $CONTAINER_NAME || rc=$?
+    check_rc $rc "Starting or attaching to Docker container"
+fi
