0905cdfb4c471436b1ad98aa3a674bbf5f763ff7
Joseph Talon <joseph.t@pervices.com>
2025-09-10 19:56:08 +0000

    Removed some sleeps

--- Testing/tests/common/engine.py
+++ Testing/tests/common/engine.py
@@ -226,7 +226,7 @@ def run(channels, wave_freq, sample_rate, center_freq, tx_gain, rx_gain, tx_stac
     print("A2")
     helper_process.start()
     print("A3")
-    time.sleep(30)
+    # time.sleep(30)
     print("A4")
 
     tx_duration = 0
@@ -244,7 +244,7 @@ def run(channels, wave_freq, sample_rate, center_freq, tx_gain, rx_gain, tx_stac
         # Expected rx duration = start time of last burst + (length of last burst / sample rate)
         rx_duration = rx_stack[-1][0] + (rx_stack[-1][1] / sample_rate)
 
-    time.sleep(40)
+    # time.sleep(40)
     print("A6")
 
     time_limit = max(tx_duration + rx_duration) + 30
