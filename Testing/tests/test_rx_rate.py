f03500371d1b15471f59d91c7c64b88cd2c77b9a
John Polak <john.p@pervices.com>
2025-09-17 14:56:07 +0000

    RX_RATE: add delay between iterations to help with host thermal

--- Testing/tests/test_rx_rate.py
+++ Testing/tests/test_rx_rate.py
@@ -3,6 +3,7 @@ from common import pdf_report
 from common import generator as gen
 from common import test_args
 import sys
+import time
 
 
 targs = test_args.TestArgs(testDesc="Rx Rate Test")
@@ -33,6 +34,7 @@ def test(it):
     # Call cpp program to run the benchmark since it is much faster and reliable
     # rx only
     if(len(it["rx_channel"]) != 0):
+        time.sleep(60) # give network card on host some time to cool down between runs
         iteration_result = os.system("/usr/lib/uhd/examples/benchmark_rate --priority high --rx_rate={} --rx_channels {}  --overrun-threshold 0 --underrun-threshold 0 --drop-threshold 0 --seq-threshold 0".format(it["rx_rate"], list_to_arg_string(it["rx_channel"])))
     # tx only
     else:
