3227d462e7d52462c23a29532dafd425a466ae21
Joseph Talon <joseph.t@pervices.com>
2025-11-26 19:18:59 +0000

    Print debug iteration

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -330,9 +330,9 @@ def tx_rx_longterm_rate():
     yield locals()
 
 def dump(iteration):
-    print(iteration)
+    log.pvpkg_log_info("GENERATOR", "", end='')
     for key, value in iteration.items():
-        log.pvpkg_log_info("GENERATOR", "%20s : %r" % (key, value))
+        log.pvpkg_log("%40s : %r" % (key, value))
 
 class cyan:
     class lo_band:
