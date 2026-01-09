e25d6b11df7168a2fd5a6123f7f1f095064354e9
Doug W. <doug.w@pervices.com>
2026-01-09 22:29:15 +0000

    TMP: PHASE 2: increasing start delay to 20

--- Testing/tests/test_rx_phase_2.py
+++ Testing/tests/test_rx_phase_2.py
@@ -30,8 +30,8 @@ import argparse
 #USER CHOSEN VALUES
 num_output_waves =1 #depends what plots look like
 begin_cutoff_waves = 1 #0.00000425 #e(-5) - guessed from previous diagrams (but seconds)
-tx_burst = 5.0 #burst should be slightly delayed to ensure all data is being collected
-rx_burst = 5.25
+tx_burst = 20.0 #burst should be slightly delayed to ensure all data is being collected
+rx_burst = 20.25
 
 std_ratio = 4  #number std gets multiplied by for checks, normalized to a sample size of 10
                #This value is adjusted later depending on the number of runs.
