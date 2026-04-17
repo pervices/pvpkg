8bca05f77f811afdb41b04ae3f9e397a03ffd946
Doug W. <doug.w@pervices.com>
2026-04-17 19:31:56 +0000

    increasing sample rate

--- Testing/tests/common/generator.py
+++ Testing/tests/common/generator.py
@@ -243,8 +243,8 @@ def hi_band_gain_rx():
 
 def lo_band_phaseCoherency():
     log.pvpkg_log_info("GENERATOR", sys._getframe().f_code.co_name)
-    sample_rate = 25000000
-    # sample_rate = 81250000/4
+    # sample_rate = 25000000
+    sample_rate = 81250000
     sample_count = int(round(sample_rate/1000))
     tx_gain = 25
     rx_gain = 25
