5ab59f6f4bd0c9b6919f743d7c817d8616c02090
Doug Wilson <doug.w@pervices.com>
2026-06-23 11:22:47 -0400

    Delay tx one second

--- Testing/tests/common/crimson.py
+++ Testing/tests/common/crimson.py
@@ -1,7 +1,19 @@
 from gnuradio import uhd
 import time
 
-def calibrate(end, channels, sample_rate, center_freq, gain):
+def calibrate_tx(end, channels, sample_rate, center_freq, gain):
+
+    end.set_samp_rate(sample_rate)
+    end.set_clock_source("internal")
+
+    for channel_index in range(len(channels)):
+        end.set_center_freq(center_freq, channel_index)
+        end.set_gain(gain, channel_index)
+
+    end.set_time_now(uhd.time_spec(0.0))
+    time.sleep(2)
+
+def calibrate_rx(end, channels, sample_rate, center_freq, gain):
 
     end.set_samp_rate(sample_rate)
     end.set_clock_source("internal")
@@ -17,13 +29,13 @@ def calibrate(end, channels, sample_rate, center_freq, gain):
 def get_snk_s(channels, sample_rate, center_freq, gain):
 
     snk = uhd.usrp_sink("crimson", uhd.stream_args(cpu_format="sc16", otw_format="sc16", channels=channels))
-    calibrate(snk, channels, sample_rate, center_freq, gain)
+    calibrate_tx(snk, channels, sample_rate, center_freq, gain)
     return snk
 
 
 def get_src_c(channels, sample_rate, center_freq, gain):
 
     src = uhd.usrp_source("crimson", uhd.stream_args(cpu_format="fc32", otw_format="sc16", channels=channels), False)
-    calibrate(src, channels, sample_rate, center_freq, gain)
+    calibrate_rx(src, channels, sample_rate, center_freq, gain)
     return src
 
